ca67c70d4e3f6ee4623704414bd56b3eb1c3726b
Edouard De Prez <deprezedouard@gmail.com>
2019-08-10 23:11:26 +0200

    added animations

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const jawgApiKey = '&access-token=w8nijIBK16cjLbibUeS9wGUMmafAPEoIOkhkb3Yl1VSt6X
 const jawgUrl = 'https://cors-anywhere.herokuapp.com/https://api.jawg.io/elevations?locations=';
 const mapBoxApiKey = '.json?access_token=pk.eyJ1IjoiZGVwcmV6ZWRvdWFyZCIsImEiOiJjanlrMTljYmIwMDRsM2xxcXpvN3d2Z3FqIn0.6TRhVAm3CyhuueZNEuCkqw';
 const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+const frontContainer = document.getElementById('front-container');
+const sea = document.getElementById('sea');
+const calculation = document.getElementById('calculation');
 const solution = document.getElementById('solution');
+let root = document.documentElement;
 let size = 0;
 let startString = 0;
 let place = '';
+let riseLevel = 29;
+
 
 
 const fetchAltitude = (input) => {
@@ -13,19 +19,25 @@ const fetchAltitude = (input) => {
   fetch(jawgUrl + input + jawgApiKey)
     .then(response => response.json())
     .then((data) => {
-      if(((data[0].elevation * 1000) + size * 10) > 7000) {
-        if(solution.classList.contains('red')) { solution.classList.remove('red');}
+      frontContainer.classList.add('slide-out');
+      let elevation = ((data[0].elevation * 1000) + size * 10);
+      if(elevation > 7000) {
+        riseLevel = 67 - Math.round((7000 * 38 / elevation));
+        console.log(riseLevel);
+        calculation.innerText = 'Calculated!';
         solution.classList.add('green');
         solution.innerHTML = `${startString}${parseInt(data[0].elevation, 10)} meters above sea level.<br> ${place} will not drown in the coming 1000 years!`;
       } else {
         const currentYear = parseInt(new Date().getFullYear(), 10);
-        const deathYear = parseInt(((((data[0].elevation * 1000) + size * 10) / 7) + currentYear));
-        if(solution.classList.contains('green')) { solution.classList.remove('green');}
-        solution.classList.add('red')
-        solution.innerHTML = `${startString}${parseInt(data[0].elevation, 10)} meters above sea level.
-        <br>
-        ${place} will drown in the year <strong>${deathYear}</strong>!`;
+        const deathYear = parseInt(((elevation / 7) + currentYear));
+        calculation.innerText = 'Calculated!';
+        solution.classList.add('red');
+        solution.innerHTML = `<div>${startString}${parseInt(data[0].elevation, 10)} meters above sea level.
+          <br>
+          ${place} will drown in the year:</div><div class="death-year">${deathYear}</div>`;
       }
+      root.style.setProperty('--result', riseLevel + "vh");
+      sea.classList.add('slide-in');
     });
 };
 
@@ -40,7 +52,7 @@ const fetchCoordinates = (input) => {
 
 const getLocation = (input) => {
   if (input !== '') {
-    place = `If you live in ${input}, you`;
+    place = `If you live in ${input}, <br> you`;
     startString = `'${input}' is currently `;
     fetchCoordinates(input);
   } else {
@@ -55,7 +67,7 @@ const getLocation = (input) => {
 const button = document.getElementById('button');
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  solution.innerHTML = 'calculating...'
+  calculation.innerHTML = 'Calculating...'
   const location = document.getElementById('location').value;
   getLocation(location);
 });
